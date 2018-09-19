6ced6b0376f86d6a69c8e755e0973b8e043f7fc8
YeMiao <15622113269@163.com>
2018-09-19 18:37:07 +0800

    9.19

--- app/src/main/java/com/ym/quickrun/model/app/MainActivity.java
+++ app/src/main/java/com/ym/quickrun/model/app/MainActivity.java
@@ -1,17 +1,7 @@
 package com.ym.quickrun.model.app;
 
-import android.content.pm.PackageManager;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.util.Log;
-import android.widget.TextView;
-
 import com.ym.quickrun.R;
 import com.ym.quickrun.base.BaseActivity;
-
-import butterknife.BindView;
 
 /**
  * @author ym
@@ -20,48 +10,48 @@ import butterknife.BindView;
  */
 public class MainActivity extends BaseActivity {
 
-    @BindView(R.id.StepCounter)
-    TextView stepCounterText;
-    private SensorManager sensorManager;
-    private Sensor stepCounter;
-    //单次步伐传感器
-    private SensorEventListener stepCounterListener;
+//    @BindView(R.id.StepCounter)
+//    TextView stepCounterText;
+//    private SensorManager sensorManager;
+//    private Sensor stepCounter;
+//    //单次步伐传感器
+//    private SensorEventListener stepCounterListener;
 
     @Override
     public void onResume() {
         super.onResume();
-        registerSensor();
+        //   registerSensor();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        unregisterSensor();
+        //    unregisterSensor();
     }
 
     @Override
     protected void initWidget() {
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        //获取传感器系统服务
-        assert sensorManager != null;
-        stepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+//        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+//        //获取传感器系统服务
+//        assert sensorManager != null;
+//        stepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
     }
 
     @Override
     protected void initData() {
-        stepCounterListener = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent event) {
-                Log.e("Counter-SensorChanged", event.values[0] + "---" + event.accuracy + "---" + event.timestamp);
-                stepCounterText.setText(new StringBuilder("总步伐计数:" + event.values[0]));
-            }
-
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {
-                Log.e("Counter-Accuracy", sensor.getName() + "---" + accuracy);
-
-            }
-        };
+//        stepCounterListener = new SensorEventListener() {
+//            @Override
+//            public void onSensorChanged(SensorEvent event) {
+//                Log.e("Counter-SensorChanged", event.values[0] + "---" + event.accuracy + "---" + event.timestamp);
+//                stepCounterText.setText(new StringBuilder("总步伐计数:" + event.values[0]));
+//            }
+//
+//            @Override
+//            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+//                Log.e("Counter-Accuracy", sensor.getName() + "---" + accuracy);
+//
+//            }
+//        };
     }
 
     @Override
@@ -69,18 +59,18 @@ public class MainActivity extends BaseActivity {
         return R.layout.activity_main;
     }
 
-    private void registerSensor() {
-        //注册传感器事件监听器
-        if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_SENSOR_STEP_COUNTER)) {
-            sensorManager.registerListener(stepCounterListener, stepCounter, SensorManager.SENSOR_DELAY_FASTEST);
-        }
-    }
-
-    private void unregisterSensor() {
-        //解注册传感器事件监听器
-        if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_SENSOR_STEP_COUNTER)) {
-            sensorManager.unregisterListener(stepCounterListener);
-        }
-    }
+//    private void registerSensor() {
+//        //注册传感器事件监听器
+//        if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_SENSOR_STEP_COUNTER)) {
+//            sensorManager.registerListener(stepCounterListener, stepCounter, SensorManager.SENSOR_DELAY_FASTEST);
+//        }
+//    }
+//
+//    private void unregisterSensor() {
+//        //解注册传感器事件监听器
+//        if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_SENSOR_STEP_COUNTER)) {
+//            sensorManager.unregisterListener(stepCounterListener);
+//        }
+//    }
 
 }
